841d1d248e3bbe038254e50a1e137be9c8fa82ba
tarobits <luigicrafter04@gmail.com>
2026-01-13 07:55:36 +0100

    feat(projects): implement automatic version fetching from npm

--- src/components/ProjectComponent.vue
+++ src/components/ProjectComponent.vue
@@ -4,7 +4,7 @@
             <img v-if="project.icon" :src="project.icon" class="aspect-square h-full">
             <div class="flex flex-col w-full">
                 <div class="flex flex-col lg:flex-row gap-3 w-full h-full">
-                <span class="text-2xl w-fit h-fit text-nowrap">{{ $t(`projects.${project.name}.title`) }}</span>
+                <span class="text-2xl w-fit h-fit text-nowrap">{{ project.title }}</span>
                 <div class="flex flex-col lg:flex-row gap-3 w-full h-full flex-wrap pb-5 lg:pb-0">
                     <SourceTagComponent :tag="project.source" />
                     <LicenseComponent v-if="project.source === 'open'" :license="project.license" />
@@ -15,11 +15,11 @@
                 </div>
             </div>
                 <p v-if="!project.released" class="text-gray-400">{{ $t('components.projectcomponent.notreleased') }}</p>
-                <p v-else-if="project.version.substring(0,2) === 'v0'" class="text-yellow-300">Version: {{ project.version }} (Pre-Release)</p>
-                <p v-else class="text-green-300">Version: {{ project.version }}</p>
+                <p v-else-if="version.substring(0,2) === 'v0'" class="text-yellow-300">Version: {{ version }} (Pre-Release)</p>
+                <p v-else class="text-green-300">Version: {{ version }}</p>
             </div>
         </div>
-        <p class="text-xl text-wrap">{{ $t(`projects.${project.name}.description`) }}</p>
+        <p class="text-xl text-wrap">{{ project.description }}</p>
         <div v-if="project.links" class="mt-auto w-full h-fit flex items-center gap-3">
             <ProjectLink v-for="l,i in project.links" :key="l.text" :next="project.links[i+1]" :link="l" />
         </div>
@@ -27,12 +27,15 @@
 </template>
 
 <script setup lang="ts">
-import { ref, toRef } from 'vue';
+import { onMounted, ref, toRef } from 'vue';
 import { ProjectType } from '../types/project';
 import LicenseComponent from './LicenseComponent.vue';
 import SourceTagComponent from './SourceTagComponent.vue';
 import PLangaugeComponent from './PLangaugeComponent.vue';
 import ProjectLink from './ProjectLink.vue';
+import axios from 'axios';
+
+const version = ref<string>("");
 
 
 const props = defineProps<{
@@ -40,6 +43,25 @@ const props = defineProps<{
 }>();
 
 const project = toRef(props.project);
+
+async function latestNPMVersion(packageName: string): Promise<string> {
+    return axios
+        .get('https://registry.npmjs.org/' + packageName + '/latest')
+        .then(res => res.data.version);
+}
+
+onMounted(async () => {
+    if (project.value.released === true) {
+        switch (project.value.versionType) {
+            case 'npm':
+                version.value = "v" + await latestNPMVersion(project.value.version)
+                break;
+            case 'static':
+                version.value = project.value.version;
+                break;
+        }
+    }
+})
 </script>
 
 <style scoped></style>
